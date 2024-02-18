a9e8063f02274bb0d00eaba812db35e0d39ca590
Md. Sabbir Al Mamon <md.sabbiralmamon@gmail.com>
2024-02-18 19:21:55 +0600

    can not select more than 4 tickets js done

--- src/js/utility.js
+++ src/js/utility.js
@@ -4,3 +4,23 @@ function goToDiv(){
     goToDiv.scrollIntoView({behavior: 'smooth'});
 }
 
+//Seat Map Section Js
+let clickCount = 0;
+document.getElementById('busSeatMapMain').addEventListener('click',function(event){
+    console.log(event.target.id);
+    const selected = document.getElementById(event.target.id);
+    if(selected.classList.contains('bg-[#1DD100]')){
+        clickCount--;
+        selected.classList.remove('bg-[#1DD100]');
+    }
+    else{
+        if(clickCount <= 3){
+            selected.classList.add('bg-[#1DD100]');
+            clickCount++;
+        }
+        else{
+            console.log('invalid');
+        }
+    }
+    console.log(clickCount);
+});
